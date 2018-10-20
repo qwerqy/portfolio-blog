224ca7b491862b0936cbec91b23f686f736cbe0b
qwerqy <amnrsln@gmail.com>
2018-10-20 19:50:02 +0800

    initial page setup

---
+++ client/src/component/pages/about/about.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    <h1>About</h1>
+  }
+}
+
+export default About;
---
+++ client/src/component/pages/blog/blog.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Blog extends Component {
+  render() {
+    <h1>Blog</h1>
+  }
+}
+
+export default Blog;
