b25810f6eef01a537b6797914be3f7076383d602
qwerqy <amnrsln@gmail.com>
2018-10-30 02:00:25 +0800

    improve UX via fixed bar on scroll

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Menu } from 'semantic-ui-react';
+import { Visibility, Container, Menu } from 'semantic-ui-react';
 import "../css/Navbar.css";
 
 class Navbar extends Component {
   constructor() {
     super()
     this.handleClick = this.handleClick.bind(this)
+    this.mountMenu = this.mountMenu.bind(this)
     this.state = {
-      activeLink: 'work'
+      activeLink: 'work',
+      mount: false
     }
   }
 
@@ -18,24 +20,42 @@ class Navbar extends Component {
     })
   }
 
+  mountMenu = () => {
+    this.setState({
+      mount: true
+    })
+  }
+
+  unmountMenu = () => {
+    this.setState({
+      mount: false
+    })
+  }
+
   render() {
-    const { activeLink } = this.state
+    const { activeLink, mount } = this.state
     return (
-      <Menu secondary pointing>
-        <Container>
-          <Menu.Item position='left' header active>Amin Roslan</Menu.Item>
-          <Menu.Item as='p' onClick={this.handleClick} active={activeLink === 'work'} id='work'>
-            <NavLink to='/'>
-              Work
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item as='p' onClick={this.handleClick} active={activeLink === 'about'} id='about'>
-            <NavLink to='/about'>
-              About
-            </NavLink>
-          </Menu.Item>
-        </Container>
-      </Menu>
+      <Visibility onBottomPassed={this.mountMenu} onBottomVisible={this.unmountMenu} once={false}>
+        <Menu className={mount ? 'borderless' : 'borderless secondary pointing'} fixed={mount && 'top'}>
+          <Container>
+            <Menu.Item as='div' position='left' header active={mount ? false : true}>
+              <NavLink to='/'>
+                Amin Roslan
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item as='div' onClick={this.handleClick} active={mount ? false : activeLink === 'work'} id='work'>
+              <NavLink to='/'>
+                Work
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item as='div' onClick={this.handleClick} active={mount ? false : activeLink === 'about'} id='about'>
+              <NavLink to='/about'>
+                About
+              </NavLink>
+            </Menu.Item>
+          </Container>
+        </Menu>
+      </Visibility>
     )
   }
 }
