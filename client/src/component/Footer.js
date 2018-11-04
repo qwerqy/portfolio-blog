8cf1e886cf0c55286785b70343e899dc55b2bd41
qwerqy <amnrsln@gmail.com>
2018-11-04 20:38:40 +0800

    added id for keys

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -6,19 +6,23 @@ class Footer extends Component {
     widths: 10,
     items: [
       {
+        id: 1,
         title: "Resume",
         link:
           "https://docs.google.com/document/d/1YGuT2nN_RMYockL5b0A6rK02uw2ZsoiQF8iCWHoQMuE/edit?usp=sharing"
       },
       {
+        id: 2,
         title: "Facebook",
         link: "https://www.facebook.com/amnrsln"
       },
       {
+        id: 3,
         title: "Github",
         link: "https://github.com/qwerqy"
       },
       {
+        id: 4,
         title: "LinkedIn",
         link: "https://www.linkedin.com/in/aminroslan/"
       }
@@ -50,7 +54,7 @@ class Footer extends Component {
           <Container textAlign="center">
             <Menu secondary hidden fluid widths={widths}>
               {items.map(item => (
-                <Menu.Item as="a" target="_blank" href={item.link}>
+                <Menu.Item key={item.id}as="a" target="_blank" href={item.link}>
                   {item.title}
                 </Menu.Item>
               ))}
