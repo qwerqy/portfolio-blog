c58d43abc563ec6a9015831ef5061099c542c911
qwerqy <amnrsln@gmail.com>
2018-11-04 19:03:18 +0800

    refactored footer hardcodies

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -2,118 +2,60 @@ import React, { Component } from "react";
 import { Icon, Responsive, Container, Divider, Menu } from "semantic-ui-react";
 
 class Footer extends Component {
+  state = {
+    widths: 10,
+    items: [
+      {
+        title: "Resume",
+        link:
+          "https://docs.google.com/document/d/1YGuT2nN_RMYockL5b0A6rK02uw2ZsoiQF8iCWHoQMuE/edit?usp=sharing"
+      },
+      {
+        title: "Facebook",
+        link: "https://www.facebook.com/amnrsln"
+      },
+      {
+        title: "Github",
+        link: "https://github.com/qwerqy"
+      },
+      {
+        title: "LinkedIn",
+        link: "https://www.linkedin.com/in/aminroslan/"
+      }
+    ]
+  };
+
+  handleWidth = width => {
+    if (width >= 992) {
+      this.setState({
+        widths: 10
+      });
+    } else if (width <= 991 && width >= 768) {
+      this.setState({
+        widths: 8
+      });
+    } else if (width <= 767 && width >= 320) {
+      this.setState({
+        widths: 4
+      });
+    }
+  };
+
   render() {
+    const { widths, items } = this.state;
     return (
       <React.Fragment>
         <Divider />
-        <Responsive {...Responsive.onlyComputer}>
+        <Responsive getWidth={() => this.handleWidth(window.innerWidth)}>
           <Container textAlign="center">
-            <Menu secondary hidden fluid widths={10}>
-              <Menu.Item
-                active
-                as="a"
-                target="_blank"
-                href="https://docs.google.com/document/d/1YGuT2nN_RMYockL5b0A6rK02uw2ZsoiQF8iCWHoQMuE/edit?usp=sharing"
-              >
-                Resumé
-              </Menu.Item>
-              <Menu.Item
-                as="a"
-                target="_blank"
-                href="https://www.facebook.com/amnrsln"
-              >
-                Facebook
-              </Menu.Item>
-              <Menu.Item
-                as="a"
-                target="_blank"
-                href="https://github.com/qwerqy"
-              >
-                Github
-              </Menu.Item>
-              <Menu.Item
-                as="a"
-                target="_blank"
-                href="https://www.linkedin.com/in/aminroslan/"
-              >
-                LinkedIn
-              </Menu.Item>
+            <Menu secondary hidden fluid widths={widths}>
+              {items.map(item => (
+                <Menu.Item as="a" target="_blank" href={item.link}>
+                  {item.title}
+                </Menu.Item>
+              ))}
             </Menu>
           </Container>
-          <Container textAlign="center">
-            <Divider hidden color="grey" />
-            <Icon name="copyright outline" /> 2018 Amin Roslan
-            <Divider hidden />
-          </Container>
-        </Responsive>
-        <Responsive {...Responsive.onlyTablet}>
-          <Container textAlign="center">
-            <Menu secondary fluid widths={8}>
-              <Menu.Item
-                active
-                as="a"
-                target="_blank"
-                href="https://docs.google.com/document/d/1YGuT2nN_RMYockL5b0A6rK02uw2ZsoiQF8iCWHoQMuE/edit?usp=sharing"
-              >
-                Resumé
-              </Menu.Item>
-              <Menu.Item
-                as="a"
-                target="_blank"
-                href="https://www.facebook.com/amnrsln"
-              >
-                Facebook
-              </Menu.Item>
-              <Menu.Item
-                as="a"
-                target="_blank"
-                href="https://github.com/qwerqy"
-              >
-                Github
-              </Menu.Item>
-              <Menu.Item
-                as="a"
-                target="_blank"
-                href="https://www.linkedin.com/in/aminroslan/"
-              >
-                LinkedIn
-              </Menu.Item>
-            </Menu>
-          </Container>
-          <Container textAlign="center">
-            <Divider hidden color="grey" />
-            <Icon name="copyright outline" /> 2018 Amin Roslan
-            <Divider hidden />
-          </Container>
-        </Responsive>
-        <Responsive {...Responsive.onlyMobile}>
-          <Menu secondary fluid widths={4}>
-            <Menu.Item
-              active
-              as="a"
-              target="_blank"
-              href="https://docs.google.com/document/d/1YGuT2nN_RMYockL5b0A6rK02uw2ZsoiQF8iCWHoQMuE/edit?usp=sharing"
-            >
-              Resumé
-            </Menu.Item>
-            <Menu.Item
-              as="a"
-              target="_blank"
-              href="https://www.facebook.com/amnrsln"
-            >
-              Facebook
-            </Menu.Item>
-            <Menu.Item as="a" target="_blank" href="https://github.com/qwerqy">
-              Github
-            </Menu.Item>
-            <Menu.Item
-              as="a"
-              target="_blank"
-              href="https://www.linkedin.com/in/aminroslan/"
-            >
-              LinkedIn
-            </Menu.Item>
-          </Menu>
           <Container textAlign="center">
             <Divider hidden color="grey" />
             <Icon name="copyright outline" /> 2018 Amin Roslan
